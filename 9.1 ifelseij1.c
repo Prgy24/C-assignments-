a236b4495efcdd644dfbdbfdcb7f63613dc64154
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2018-12-26 11:24:22 +0530

    edited and updated

--- 9.1 ifelseij1.c
+++ 9.1 ifelseij1.c
@@ -1,4 +1,5 @@
-/*using condition operator( if-else)*/
+/*Develop a C program having following logic. If i is 20 or j is 20, display as “Atleast one variable is having 20” otherwise display “Both variables are not having 20” . If i is less than or equal to 40 and j is less than or equal to 40, It should display “Both are less than or equal to 40” otherwise, it should display as “Both are not less than or equal to 40” . Implement this using  with conditional operator. 
+ */
 
 #include<stdio.h>
 
