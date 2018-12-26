e6a942a85bcf1e1ef74af11c854268a43c783b3b
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2018-12-26 10:25:17 +0530

    program heading added

--- 1.operation.c
+++ 1.operation.c
@@ -1,3 +1,7 @@
+
+/*Develop a C program to perform operations (+,*,-, / and %) on two whole numbers. Identify suitable data types to represent the numbers and resultant values */
+
+
 #include<stdio.h>
 int main()
 {
